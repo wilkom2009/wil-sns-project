e8a9eb79512b672cf0232cfc24c55a3534b856fa
Wilson <wilkom2009@yahoo.fr>
2020-10-03 15:21:53 +0000

    Update Readme.md

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const IndexPage = () => (
       <SubscribeForm />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Link to="/using-typescript/">Go to "Using TypeScript" </Link>
   </Layout>
 )
 
